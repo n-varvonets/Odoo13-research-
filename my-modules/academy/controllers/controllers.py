a68f489001891c62a5b466a4afacca715d0ae96e
nick <nickolay.varvonets@gmail.com>
2021-04-02 12:21:03 +0300

    first commit

---
+++ my-modules/academy/controllers/controllers.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+# ./odoo-bin --addons-path addons,my-modules
+
+class Academy(http.Controller):
+    @http.route('/academy/academy/', auth='public', website=True)
+    def index(self, **kw):
+        Teachers = http.request.env['academy.teachers']
+        return http.request.render('academy.index', {
+            'teachers': Teachers.search([])
+        })
+
+    # """route for local dynamic variables"""
+    # @http.route('/academy/<name>/', auth='public', website=True)
+    # def teacher(self, name):
+    #     return '<h1>{}</h1>'.format(name)
+    #
+    # """route for dynamic int """
+    # @http.route('/academy/<int:id>/', auth='public', website=True)
+    # def teacher(self, id):
+    #     return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
+
+    """provides records directly when given their id"""
+    @http.route('/academy/<model("academy.teachers"):teacher>/', auth='public', website=True)
+    def teacher(self, teacher):
+        return http.request.render('academy.biography', {
+            'person': teacher
+        })
+
+
+
+#     @http.route('/academy/academy/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('academy.listing', {
+#             'root': '/academy/academy',
+#             'objects': http.request.env['academy.academy'].search([]),
+#         })
+
+#     @http.route('/academy/academy/objects/<model("academy.academy"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('academy.object', {
+#             'object': obj
+#         })
---
+++ my-modules/academy/models/models.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Teachers(models.Model):
+    _name = 'academy.teachers'
+
+    name = fields.Char()
+    biography = fields.Html()
+
+    """Также должна быть возможность создавать новые курсы прямо со страницы учителя или видеть все курсы,
+         которые они преподают, поэтому добавьте обратную зависимость к модели учителя:"""
+    course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+
+
+class Courses(models.Model):
+    """У каждого курса должно быть поле учитель, связанное с одной записью учителя,
+    но каждый учитель может вести несколько курсов"""
+    _name = 'academy.courses'
+
+    name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
+
+
+# class academy(models.Model):
+#     _name = 'academy.academy'
+#     _description = 'academy.academy'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
