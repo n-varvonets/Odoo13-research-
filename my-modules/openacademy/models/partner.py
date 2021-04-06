ad646cd8ad1f5643417701f2ec15057b64839db7
nick <nickolay.varvonets@gmail.com>
2021-04-06 16:03:40 +0300

    passed domains

---
+++ my-modules/openacademy/models/partner.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class Partner(models.Model):
+    """Using model inheritance, modify the existing Partner model to add an instructor boolean field,
+     and a many2many field that corresponds to the session-partner relation"""
+    _inherit = 'res.partner'
+
+    # Add a new column to the res.partner model, by default partners are not
+    # instructors
+    instructor = fields.Boolean("Instructor", default=False)
+
+    session_ids = fields.Many2many('openacademy.session',
+        string="Attended Sessions", readonly=True)
