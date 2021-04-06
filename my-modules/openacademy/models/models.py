821f8d9133d9587614135c88c299d8260065a190
nick <nickolay.varvonets@gmail.com>
2021-04-06 09:41:08 +0300

    passed Relations between models for openacdemy

--- my-modules/openacademy/models/models.py
+++ my-modules/openacademy/models/models.py
@@ -9,6 +9,16 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)  # то как будет называться титл кусра(для различных способов отображения и поиска)
     description = fields.Text()
 
+
+class Session(models.Model):  # a session is an occurrence of a course taught at a given time for a given audience
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")  # плавающей запятой: 6 - это общее количество цифр, а 2 - количество цифр после запятой. Обратите внимание, что в результате количество цифр перед запятой составляет максимум 4
+    seats = fields.Integer(string="Number of seats")
+
 #     name = fields.Char()
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
