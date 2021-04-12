7355ef26e033e16b32e480e97cb5c288be242d87
nick <nickolay.varvonets@gmail.com>
2021-04-12 12:21:42 +0300

    passed set by default

--- my_addons/openacademy/models/models.py
+++ my_addons/openacademy/models/models.py
@@ -28,9 +28,10 @@ class Session(models.Model):
     _description = "OpenAcademy Sessions"
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
 
     # у каждой сессии(прошлой,нынешный, следующие, паралелльной - many)  может быть только один ответсвенный за
     # ее проведения(ректор), а если новый  ректор, то у него будет свои сессии, но ректор ТОЛЬКО ОДИН поэтмоу Many2one
