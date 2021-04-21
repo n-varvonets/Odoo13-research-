6e730cc6a1804075d513279fce2ad493615a8818
nick <nickolay.varvonets@gmail.com>
2021-04-21 17:21:07 +0300

    Handling Of Multiples Views For Same Model for instagram module by posts(form/tree) and info(form/tree)

--- my_addons/instagram/controllers/controllers.py
+++ my_addons/instagram/controllers/controllers.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+from my_addons.instagram.instascraper import *
 
 
+class Instagram(http.Controller):
+    @http.route('/instpost_webform', type="http", auth="public", website=True)
+    def instpost_webform(self, **kw):
+        """здесь мы можем предварительно брать данные с базы и брасать их в наш шаблок перед редиректом по роуту"""
+        # print("Execution Here.........................", kw)
+        # doctor_rec = request.env['hospital.doctor'].sudo().search([])
+        # print("doctor_rec...", doctor_rec)
+        return http.request.render('instagram.get_info_post', {"qty_posts": 3})
 
-# class Instagram(http.Controller):
-#     @http.route('/patient_webform', type="http", auth="public", website=True)
-#     def patient_webform(self, **kw):
-#         print("Execution Here.........................")
-#         doctor_rec = request.env['hospital.doctor'].sudo().search([])
-#         print("doctor_rec...", doctor_rec)
-#         return http.request.render('om_hospital.create_patient', {'patient_name': 'Odoo Mates Test 123',
-#                                                                   'doctor_rec': doctor_rec})
-#
-#     @http.route('/create/webpatient', type="http", auth="public", website=True)
-#     def create_webpatient(self, **kw):
-#         print("Data Received.....", kw)
-#         request.env['hospital.patient'].sudo().create(kw)
-#         # doctor_val = {
-#         #     'name': kw.get('patient_name')
-#         # }
-#         # request.env['hospital.doctor'].sudo().create(doctor_val)
-#         return request.render("om_hospital.patient_thanks", {})
+    @http.route('/create/webposts', type="http", auth="public", website=True)
+    def create_webpatient(self, **kw):
+        """здесь мы получаем данные с темплейта, их обрабатываем и передаем дальше"""
+        print("Data Received.....", kw)
+
+        request.env['instagram.instagram'].sudo().create(kw)
+
+
+        # doctor_val = {
+        #     'name': kw.get('patient_name')
+        # }
+        # request.env['hospital.doctor'].sudo().create(doctor_val)
+        return request.render("instagram.success_msg_and_data", {})
--- my_addons/instagram/models/models.py
+++ my_addons/instagram/models/models.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from my_addons.instagram.instascraper import *
 from odoo import models, fields, api
 
 
@@ -7,13 +6,13 @@ class Instagram(models.Model):
     _name = 'instagram.instagram'
     _description = 'instagram.instagram'
 
-    name = fields.Char(string="Instagram model", required=True)
+    name = fields.Char(string="Instagram model")
 
-    # username = fields.Char(string="Your username", required=True)
-    # user_login = fields.Char(string="Input your email/phone for authorization in instagram", required=True)
-    # user_pass = fields.Char(string="Input your password for authorization in instagram", required=True)
-    # qty_posts = fields.Integer(string="Input the number of posts you want to find", default=3)
-    # required_acc_to_find = fields.Char(string="Input name of instagram account who you are looking for", required=True)
+    username = fields.Char(string="Name", required=True)
+    user_login = fields.Char(string="User's email/phone", required=True)
+    user_pass = fields.Char(string="User's password", required=True)
+    qty_posts = fields.Integer(string="Number of posts", default=3)
+    required_acc_to_find = fields.Char(string="Nickname to search in instagram", required=True)
 
     # id_post = fields.Char(string="ID of post", store=True)
     # img = fields.Char(string="URL img", store=True)
